8ef7ffcf0e3c7e5dee329f488c629bc2edcb8b92
Chris Schmidt <shinobistruth@gmail.com>
2025-03-06 14:44:54 -0500

    one switch with low-res dimmer

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,7 @@ using std::isnan;
 using std::min;
 using std::max;
 using namespace switch_;
+using namespace light;
 logger::Logger *logger_logger_id;
 web_server_base::WebServerBase *web_server_base_webserverbase_id;
 captive_portal::CaptivePortal *captive_portal_captiveportal_id;
@@ -18,32 +19,39 @@ using namespace api;
 preferences::IntervalSyncer *preferences_intervalsyncer_id;
 gpio::GPIOSwitch *gpio_gpioswitch_id;
 esp32::ESP32InternalGPIOPin *esp32_esp32internalgpiopin_id;
+using namespace output;
+esp32::ESP32InternalGPIOPin *esp32_esp32internalgpiopin_id_2;
+ledc::LEDCOutput *gpio_5;
+monochromatic::MonochromaticLightOutput *monochromatic_monochromaticlightoutput_id;
+light::LightState *light_lightstate_id;
 // ========== AUTO GENERATED INCLUDE BLOCK END ==========="
 
 void setup() {
   // ========== AUTO GENERATED CODE BEGIN ===========
   // esphome:
-  //   name: 4-port-relay
-  //   friendly_name: 4 Port Relay
-  //   min_version: 2024.12.4
-  //   build_path: build/4-port-relay
+  //   name: swaggest
+  //   friendly_name: Swaggest
+  //   min_version: 2025.2.2
+  //   build_path: build/swaggest
   //   area: ''
   //   platformio_options: {}
   //   includes: []
   //   libraries: []
   //   name_add_mac_suffix: false
-  App.pre_setup("4-port-relay", "4 Port Relay", "", "", __DATE__ ", " __TIME__, false);
+  App.pre_setup("swaggest", "Swaggest", "", "", __DATE__ ", " __TIME__, false);
   // switch:
+  // light:
   // logger:
   //   level: VERBOSE
   //   id: logger_logger_id
   //   baud_rate: 115200
   //   tx_buffer_size: 512
   //   deassert_rts_dtr: false
-  //   hardware_uart: UART0
+  //   hardware_uart: USB_SERIAL_JTAG
   //   logs: {}
   logger_logger_id = new logger::Logger(115200, 512);
-  logger_logger_id->set_uart_selection(logger::UART_SELECTION_UART0);
+  logger_logger_id->set_log_level(ESPHOME_LOG_LEVEL_VERBOSE);
+  logger_logger_id->set_uart_selection(logger::UART_SELECTION_USB_SERIAL_JTAG);
   logger_logger_id->pre_setup();
   logger_logger_id->set_component_source("logger");
   App.register_component(logger_logger_id);
@@ -60,8 +68,8 @@ void setup() {
   App.register_component(captive_portal_captiveportal_id);
   // wifi:
   //   ap:
-  //     ssid: 4-Port-Relay Fallback Hotspot
-  //     password: Tp6KpfFp1KtZ
+  //     ssid: Swaggest Fallback Hotspot
+  //     password: OTpu9XNYcBfM
   //     id: wifi_wifiap_id
   //     ap_timeout: 1min
   //   id: wifi_wificomponent_id
@@ -78,9 +86,9 @@ void setup() {
   //     password: !secret 'wifi_password'
   //     id: wifi_wifiap_id_2
   //     priority: 0.0
-  //   use_address: 4-port-relay.local
+  //   use_address: swaggest.local
   wifi_wificomponent_id = new wifi::WiFiComponent();
-  wifi_wificomponent_id->set_use_address("4-port-relay.local");
+  wifi_wificomponent_id->set_use_address("swaggest.local");
   {
   wifi::WiFiAP wifi_wifiap_id_2 = wifi::WiFiAP();
   wifi_wifiap_id_2.set_ssid("LFG");
@@ -90,8 +98,8 @@ void setup() {
   }
   {
   wifi::WiFiAP wifi_wifiap_id = wifi::WiFiAP();
-  wifi_wifiap_id.set_ssid("4-Port-Relay Fallback Hotspot");
-  wifi_wifiap_id.set_password("Tp6KpfFp1KtZ");
+  wifi_wifiap_id.set_ssid("Swaggest Fallback Hotspot");
+  wifi_wifiap_id.set_password("OTpu9XNYcBfM");
   wifi_wificomponent_id->set_ap(wifi_wifiap_id);
   }
   wifi_wificomponent_id->set_ap_timeout(60000);
@@ -112,13 +120,13 @@ void setup() {
   // ota:
   // ota.esphome:
   //   platform: esphome
-  //   password: 350cbc745a72ad42a9230af55f4fa439
+  //   password: 4662fe250be84e74cf8a3c14ea83a99b
   //   id: esphome_esphomeotacomponent_id
   //   version: 2
   //   port: 3232
   esphome_esphomeotacomponent_id = new esphome::ESPHomeOTAComponent();
   esphome_esphomeotacomponent_id->set_port(3232);
-  esphome_esphomeotacomponent_id->set_auth_password("350cbc745a72ad42a9230af55f4fa439");
+  esphome_esphomeotacomponent_id->set_auth_password("4662fe250be84e74cf8a3c14ea83a99b");
   esphome_esphomeotacomponent_id->set_component_source("esphome.ota");
   App.register_component(esphome_esphomeotacomponent_id);
   // safe_mode:
@@ -133,7 +141,7 @@ void setup() {
   if (safe_mode_safemodecomponent_id->should_enter_safe_mode(10, 300000, 60000)) return;
   // api:
   //   encryption:
-  //     key: Hq03eLh5CVx4Ej3n4yeKvdHfxVWJOvaIMTgF4KeiGJo=
+  //     key: WCh9duAvxF3hpHWJAMsXhLMEblRwgtY8HCphYSm8GiY=
   //   id: api_apiserver_id
   //   port: 6053
   //   password: ''
@@ -144,7 +152,7 @@ void setup() {
   api_apiserver_id->set_port(6053);
   api_apiserver_id->set_password("");
   api_apiserver_id->set_reboot_timeout(900000);
-  api_apiserver_id->set_noise_psk({30, 173, 55, 120, 184, 121, 9, 92, 120, 18, 61, 231, 227, 39, 138, 189, 209, 223, 197, 85, 137, 58, 246, 136, 49, 56, 5, 224, 167, 162, 24, 154});
+  api_apiserver_id->set_noise_psk({88, 40, 125, 118, 224, 47, 196, 93, 225, 164, 117, 137, 0, 203, 23, 132, 179, 4, 110, 84, 112, 130, 214, 60, 28, 42, 97, 97, 41, 188, 26, 38});
   // esp32:
   //   board: esp32-c6-devkitc-1
   //   flash_size: 8MB
@@ -167,6 +175,7 @@ void setup() {
   App.register_component(preferences_intervalsyncer_id);
   // switch.gpio:
   //   platform: gpio
+  //   name: Switch 1
   //   pin:
   //     number: 4
   //     mode:
@@ -180,15 +189,14 @@ void setup() {
   //     ignore_pin_validation_error: false
   //     ignore_strapping_warning: false
   //     drive_strength: 20.0
-  //   name: Relay 1
   //   disabled_by_default: false
   //   restore_mode: ALWAYS_OFF
   //   id: gpio_gpioswitch_id
   //   interlock_wait_time: 0ms
   gpio_gpioswitch_id = new gpio::GPIOSwitch();
   App.register_switch(gpio_gpioswitch_id);
-  gpio_gpioswitch_id->set_name("Relay 1");
-  gpio_gpioswitch_id->set_object_id("relay_1");
+  gpio_gpioswitch_id->set_name("Switch 1");
+  gpio_gpioswitch_id->set_object_id("switch_1");
   gpio_gpioswitch_id->set_disabled_by_default(false);
   gpio_gpioswitch_id->set_restore_mode(switch_::SWITCH_ALWAYS_OFF);
   gpio_gpioswitch_id->set_component_source("gpio.switch");
@@ -199,6 +207,60 @@ void setup() {
   esp32_esp32internalgpiopin_id->set_drive_strength(::GPIO_DRIVE_CAP_2);
   esp32_esp32internalgpiopin_id->set_flags(gpio::Flags::FLAG_OUTPUT);
   gpio_gpioswitch_id->set_pin(esp32_esp32internalgpiopin_id);
+  // output:
+  // output.ledc:
+  //   platform: ledc
+  //   pin:
+  //     number: 5
+  //     mode:
+  //       output: true
+  //       input: false
+  //       open_drain: false
+  //       pullup: false
+  //       pulldown: false
+  //     id: esp32_esp32internalgpiopin_id_2
+  //     inverted: false
+  //     ignore_pin_validation_error: false
+  //     ignore_strapping_warning: false
+  //     drive_strength: 20.0
+  //   id: gpio_5
+  //   frequency: 1000.0
+  //   zero_means_zero: false
+  esp32_esp32internalgpiopin_id_2 = new esp32::ESP32InternalGPIOPin();
+  esp32_esp32internalgpiopin_id_2->set_pin(::GPIO_NUM_5);
+  esp32_esp32internalgpiopin_id_2->set_inverted(false);
+  esp32_esp32internalgpiopin_id_2->set_drive_strength(::GPIO_DRIVE_CAP_2);
+  esp32_esp32internalgpiopin_id_2->set_flags(gpio::Flags::FLAG_OUTPUT);
+  gpio_5 = new ledc::LEDCOutput(esp32_esp32internalgpiopin_id_2);
+  gpio_5->set_component_source("ledc.output");
+  App.register_component(gpio_5);
+  gpio_5->set_zero_means_zero(false);
+  gpio_5->set_frequency(1000.0f);
+  // light.monochromatic:
+  //   platform: monochromatic
+  //   output: gpio_5
+  //   name: ESP32 Dimmer
+  //   disabled_by_default: false
+  //   id: light_lightstate_id
+  //   restore_mode: ALWAYS_OFF
+  //   gamma_correct: 2.8
+  //   default_transition_length: 1s
+  //   flash_transition_length: 0s
+  //   output_id: monochromatic_monochromaticlightoutput_id
+  monochromatic_monochromaticlightoutput_id = new monochromatic::MonochromaticLightOutput();
+  light_lightstate_id = new light::LightState(monochromatic_monochromaticlightoutput_id);
+  App.register_light(light_lightstate_id);
+  light_lightstate_id->set_component_source("light");
+  App.register_component(light_lightstate_id);
+  light_lightstate_id->set_name("ESP32 Dimmer");
+  light_lightstate_id->set_object_id("esp32_dimmer");
+  light_lightstate_id->set_disabled_by_default(false);
+  light_lightstate_id->set_restore_mode(light::LIGHT_ALWAYS_OFF);
+  light_lightstate_id->set_default_transition_length(1000);
+  light_lightstate_id->set_flash_transition_length(0);
+  light_lightstate_id->set_gamma_correct(2.8f);
+  light_lightstate_id->add_effects({});
+  monochromatic_monochromaticlightoutput_id->set_output(gpio_5);
   // socket:
   //   implementation: bsd_sockets
   // md5:
